85f12ac4a7d0e9825c4c76457032bb2f179826b6
Albert Deuzeman <deuzeman@itp.unibe.ch>
2011-03-03 14:53:02 +0000

    Polish of canonical.c
    git-svn-id: svn://thep.housing.rug.nl/lemon@30 038c52d2-56c5-4035-8d8a-1cd00843ba61

--- check/canonical.c
+++ check/canonical.c
@@ -5,27 +5,46 @@
 
 int main(int argc, char **argv)
 {
-  int rank;
   MPI_File fp;
-  MPI_Info info;
+  MPI_Comm cartesian;
+
+  /* Needed for the creation of a Cartesian communicator */
+  int mpiSize;
+  int distribution[] = {0, 0, 0, 0};
+  int periods[] = {1, 1, 1, 1};
+
   LemonWriter *w;
   LemonRecordHeader *h;
+
+  /* The data we want to write */
   char message[] = "LEMON test message";
   MPI_Offset bytes = strlen(message);
 
   MPI_Init(&argc, &argv);
-  MPI_Info_create(&info);
 
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_File_open(MPI_COMM_WORLD, "canonical.test", MPI_MODE_WRONLY | MPI_MODE_CREATE, info, &fp);
+  /* Create a 4 dimensional Cartesian node distribution. */
+  MPI_Comm_size(MPI_COMM_WORLD, &mpiSize);
+  MPI_Dims_create(mpiSize, 4 /* #dims */, distribution);
+  MPI_Cart_create(MPI_COMM_WORLD, 4 /* #dims */ , distribution, periods, 1 /* reorder */, &cartesian);
+
+  /* Open the file using the Cartesian communicator just created, initialize a writer with it */
+  MPI_File_open(MPI_COMM_WORLD, "canonical.test", MPI_MODE_WRONLY | MPI_MODE_CREATE, MPI_INFO_NULL, &fp);
   w = lemonCreateWriter(&fp, MPI_COMM_WORLD);
-  h = lemonCreateHeader(1, 1, "lemon-test-text", bytes);
 
+  /* Create a header, write it out and destroy it again */
+  h = lemonCreateHeader(1 /* MB */, 1 /* ME */, "lemon-test-text", bytes);
   lemonWriteRecordHeader(h, w);
-  lemonWriteRecordData(message, &bytes, w);
   lemonDestroyHeader(h);
+
+  /* Write out the small test string defined earlier as the data block */
+  lemonWriteRecordData(message, &bytes, w);
+  lemonWriterCloseRecord(w);
+
+  /* Close the writer and release resources */
   lemonDestroyWriter(w);
   MPI_File_close(&fp);
+
   MPI_Finalize();
+
   return 0;
 }
